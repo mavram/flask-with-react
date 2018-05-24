e34268645509a7b77dc9b7d86b00c9d254464ca5
mavram <mavram@gmail.com>
2018-05-24 00:47:38 -0400

    basic-logging

--- flask_with_react/app.py
+++ flask_with_react/app.py
@@ -1,22 +1,70 @@
 
+import json
+import logging
+from logging.handlers import RotatingFileHandler
 import os
-from flask import Flask
-from .config import get_env_config
+import functools
+from flask import Flask, Config
+
+APP_ROOT = os.path.dirname(__file__)
+
+@functools.lru_cache()
+def get_environment_config():
+    env = os.environ['FLASK_ENV'] if 'FLASK_ENV' in os.environ else None
+    if not env:
+        raise ValueError('Missing environment definition.')
+    c = Config(os.path.join(os.path.dirname(__file__), 'config'))
+    try:
+        c.from_pyfile(f'default.py')
+        c.from_pyfile(f'{env}.py')
+    except FileNotFoundError:
+        raise ValueError(f'Unknown environment. {env}')
+    return c
+
+@functools.lru_cache()
+def get_config_param(name, default=None):
+    if name in os.environ:
+        return os.environ[name]
+    elif name in get_environment_config():
+        return get_environment_config()[name]
+    elif default is not None:
+        return default
+    raise ValueError(f'Unknown configuration parameter. {name}')
 
 
 def get_app():
-    web_assets_folder = os.path.join(*[os.path.dirname(os.path.abspath(__file__)), 'webapp', 'static'])
-    c = get_env_config()
+    web_assets_folder = os.path.join(*[APP_ROOT, 'webapp', 'static'])
     a = Flask(
         __name__.split('.')[0],
         static_folder=web_assets_folder,
         template_folder=web_assets_folder)
-    a.config.from_mapping(c)
+    a.config.from_mapping(get_environment_config())
+
+    def get_log_level():
+        return logging.DEBUG if a.config['DEBUG'] else logging.INFO
+
+    def get_log_handlers():
+        formatter = logging.Formatter('%(asctime)s %(process)s %(pathname)s:%(lineno)d: %(message)s')
+        handlers = []
+        fh = RotatingFileHandler(get_config_param('LOG_PATH'), backupCount=3)
+        fh.setLevel(get_log_level())
+        fh.setFormatter(formatter)
+        handlers.append(fh)
+        if a.config['DEBUG']:
+            ch = logging.StreamHandler()
+            ch.setLevel(logging.DEBUG)
+            ch.setFormatter(formatter)
+            handlers.append(ch)
+        return handlers
+
+    logger = logging.getLogger(a.name)
+    logger.setLevel(get_log_level())
+    _ = [logger.addHandler(h) for h in get_log_handlers()]
+
+    with open(os.path.join(APP_ROOT, 'app.json')) as f:
+        app_info = json.load(f)
+    logging.getLogger(__name__).info('%s-%s: %s', app_info['name'], app_info['version'], a.config['ENV'])
+
     return a
 
-
 app = get_app()
-
-@app.before_first_request
-def at_startup():
-    print('Started...')
--- flask_with_react/config/default.py
+++ flask_with_react/config/default.py
@@ -1,2 +1,5 @@
 
+import os
+
 PORT = 8080
+LOG_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'app.log')
