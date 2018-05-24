418a249ef901f1cfc6e1f14200b2a7f429c9be66
mavram <mavram@gmail.com>
2018-05-23 23:35:52 -0400

    code-formatting

--- flask_with_react/app.py
+++ flask_with_react/app.py
@@ -6,12 +6,17 @@ from .config import get_env_config
 
 def get_app():
     web_assets_folder = os.path.join(*[os.path.dirname(os.path.abspath(__file__)), 'webapp', 'static'])
+    c = get_env_config()
     a = Flask(
         __name__.split('.')[0],
         static_folder=web_assets_folder,
         template_folder=web_assets_folder)
-    a.config.from_mapping(get_env_config())
+    a.config.from_mapping(c)
     return a
 
 
-app = get_app()  # pylint: disable=invalid-name
+app = get_app()
+
+@app.before_first_request
+def at_startup():
+    print('Started...')
